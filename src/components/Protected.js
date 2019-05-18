2d1346b9a27d978d8f253a364ff4223d8cdcd08a
Max Euler <maxjuliuseuler@gmail.com>
2019-05-18 14:58:27 +0200

    Add protected component

---
+++ src/components/Protected.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Protected = props => (
+	<div>
+		<h2>Hello There!</h2>
+		<p>You can see that because you are logged in!</p>
+	</div>
+);
+
+export default Protected;
